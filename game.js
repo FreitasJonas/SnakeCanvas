a0df63210a0d7237611044ae0917f8e06a125119
EstecTecnologia <jonas@estec.com.br>
2019-12-04 15:12:57 -0300

    matriz

--- game.js
+++ game.js
@@ -1,7 +1,10 @@
 const CANVAS_ID = "game";
 
-const ALTURA = 600;
 const LARGURA = 400;
+const ALTURA = 600;
+
+const COLUNAS = 12;
+const LINHAS = 24;
 
 const BG_COLOR = "#000000";
 const CEL_COLOR = "#C0C0C0";
@@ -12,13 +15,18 @@ const BAIXO = 40;
 const DIREITA = 39;
 const ESQUERDA = 37;
 
-dificuldade = 5;
+dificuldade = 10;
 
 snakeDir = DIREITA;
-snakeCelSize = { height: (ALTURA / 100), width: (LARGURA / 100) }
-snakePosition = { x: 100, y: 100};
+snakeCelSize = { height: (ALTURA / 50), width: (LARGURA / 50) }
+
+snakePosition = { col: 10, line: 10};
+snake = [];
 
 applePosition = { x: Math.floor(Math.random() * (LARGURA)), y: 100 }
+
+tabuleiro = [];
+celSize = 50;
 
 window.onload = function(e) {
     console.log("Ready");
@@ -29,12 +37,15 @@ window.onload = function(e) {
 
     let ctx = document.getElementById(CANVAS_ID).getContext("2d");
     ctx.canvas.width = LARGURA;
-    ctx.canvas.height = ALTURA;    
+    ctx.canvas.height = ALTURA;   
+    
+    snake.push(snakePosition);
+
 
     window.setInterval(function(e) {
         
         pintarQuadro();
-        processar();
+        //processar();
         pintar();
 
     }, (1000 / dificuldade))
@@ -57,34 +68,39 @@ function movimentar(event) {
 }
 
 function processar() {
-    
+
     //movimentacao snake
     if(snakeDir == DIREITA) {
-        snakePosition.x += snakeCelSize.width;
+        snake[0].x += snakeCelSize.width;
     }
     else if(snakeDir == ESQUERDA) {
-        snakePosition.x -= snakeCelSize.width;
+        snake[0].x -= snakeCelSize.width;
     }
     else if(snakeDir == CIMA) {
-        snakePosition.y -= snakeCelSize.height;
+        snake[0].y -= snakeCelSize.height;
     }
     else if(snakeDir == BAIXO) {
-        snakePosition.y += snakeCelSize.height;
+        snake[0].y += snakeCelSize.height;
+    }
+    
+    for(let i = 1; i < snake.length; i++) {
+        
+        if(snake[i] != null && snake[i] != undefined) {
+            
+            snake[i] = snake[i - 1];
+        }
     }
 
-    console.log("S " + JSON.stringify(snakePosition));
-    console.log("A" + JSON.stringify(applePosition));
+    let xPos = snake[0].x - applePosition.x;
+    let yPos = snake[0].y - applePosition.y;
 
-    //aplle respawn
-    let xPos = snakePosition.x - applePosition.x;
-    let yPos = snakePosition.y - applePosition.y;
+    let rangeX = 10;
+    let rangeY = 20;
 
-    let rangeX = 2;
-    let rangeY = 2;
-
-    if((xPos > 0 && xPos < rangeX) && (yPos > 0 && yPos < rangeY)) {
+    if((xPos >= 0 && xPos < rangeX) && (yPos >= 0 && yPos < rangeY)) {
         applePosition = { x: Math.floor(Math.random() * (LARGURA)), y: Math.floor(Math.random() * (ALTURA)) }
-    }       
+        snake.push({ x: snake[0].x + snakeCelSize.width, y: snake[0].y + snakeCelSize.height })
+    } 
 }
 
 function pintar() {
@@ -92,7 +108,14 @@ function pintar() {
     //pintar snake
     let ctx = document.getElementById(CANVAS_ID).getContext("2d");
     ctx.fillStyle = CEL_COLOR;
-    ctx.fillRect(snakePosition.x, snakePosition.y, snakeCelSize.width, snakeCelSize.height);
+
+    for(let i = 0; i < snake.length; i++) {
+        
+        if(snake[i] != null && snake[i] != undefined) {
+
+            ctx.fillRect(snake[i].col, snake[i].line, snakeCelSize.width, snakeCelSize.height);
+        }
+    }    
 
     //pintar apple
     ctx.fillStyle = APPLE_COLOR;
@@ -100,8 +123,17 @@ function pintar() {
 }
 
 function pintarQuadro() {
-    
+
     let ctx = document.getElementById(CANVAS_ID).getContext("2d");
-    ctx.fillStyle = BG_COLOR;
-    ctx.fillRect(0, 0, LARGURA, ALTURA);
+        
+    for(let i = 0; i < COLUNAS; i++) {
+        for(let j = 0; j < LINHAS; j++) {
+            
+            let x = i * celSize;
+            let y = j * celSize;
+
+            ctx.fillStyle = BG_COLOR;
+            ctx.fillRect(0, 0, LARGURA, ALTURA);                        
+        }
+    }
 }
