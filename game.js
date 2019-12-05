cbb7afa64eae55b845fe612416333748b7d565cc
EstecTecnologia <jonas@estec.com.br>
2019-12-05 17:27:17 -0300

    snake body

--- game.js
+++ game.js
@@ -23,7 +23,7 @@ snakeCelSize = { height: (ALTURA / 50), width: (LARGURA / 50) }
 snakePosition = { col: 10, line: 10};
 snake = [];
 
-applePosition = { x: Math.floor(Math.random() * (LARGURA)), y: 100 }
+applePosition = { col: 10, line: 20};
 
 tabuleiro = [];
 celSize = 50;
@@ -40,12 +40,12 @@ window.onload = function(e) {
     ctx.canvas.height = ALTURA;   
     
     snake.push(snakePosition);
-
+    snake.push(snakePosition);
 
     window.setInterval(function(e) {
         
         pintarQuadro();
-        //processar();
+        processar();
         pintar();
 
     }, (1000 / dificuldade))
@@ -69,38 +69,32 @@ function movimentar(event) {
 
 function processar() {
 
+    let headPosition = snake[0];
+
     //movimentacao snake
     if(snakeDir == DIREITA) {
-        snake[0].x += snakeCelSize.width;
+        snake[0].col += 1; //snakeCelSize.width;
     }
     else if(snakeDir == ESQUERDA) {
-        snake[0].x -= snakeCelSize.width;
+        snake[0].col -= 1; //snakeCelSize.width;
     }
     else if(snakeDir == CIMA) {
-        snake[0].y -= snakeCelSize.height;
+        snake[0].line -= 1; //snakeCelSize.height;
     }
     else if(snakeDir == BAIXO) {
-        snake[0].y += snakeCelSize.height;
+        snake[0].line += 1; //snakeCelSize.height;
     }
     
     for(let i = 1; i < snake.length; i++) {
         
         if(snake[i] != null && snake[i] != undefined) {
             
-            snake[i] = snake[i - 1];
+            snake[i].col = headPosition.col -1;
+            snake[i].line = headPosition.line -1;
         }
-    }
+    }    
 
-    let xPos = snake[0].x - applePosition.x;
-    let yPos = snake[0].y - applePosition.y;
-
-    let rangeX = 10;
-    let rangeY = 20;
-
-    if((xPos >= 0 && xPos < rangeX) && (yPos >= 0 && yPos < rangeY)) {
-        applePosition = { x: Math.floor(Math.random() * (LARGURA)), y: Math.floor(Math.random() * (ALTURA)) }
-        snake.push({ x: snake[0].x + snakeCelSize.width, y: snake[0].y + snakeCelSize.height })
-    } 
+    console.log(snake);
 }
 
 function pintar() {
@@ -113,13 +107,15 @@ function pintar() {
         
         if(snake[i] != null && snake[i] != undefined) {
 
-            ctx.fillRect(snake[i].col, snake[i].line, snakeCelSize.width, snakeCelSize.height);
+            
+
+            ctx.fillRect(snake[i].col * snakeCelSize.width, snake[i].line * snakeCelSize.height, snakeCelSize.width, snakeCelSize.height);
         }
     }    
 
     //pintar apple
     ctx.fillStyle = APPLE_COLOR;
-    ctx.fillRect(applePosition.x, applePosition.y, snakeCelSize.width, snakeCelSize.height);
+    ctx.fillRect(applePosition.col * snakeCelSize.width, applePosition.line * snakeCelSize.height, snakeCelSize.width, snakeCelSize.height);
 }
 
 function pintarQuadro() {
